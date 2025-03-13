08e47aa4a7396d2ad9385c1869406603cc6346c4
Nil1620 <nilesh.baglekar@gmail.com>
2025-03-13 13:37:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function openTab(event, tabName) {
+    let i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove("active");
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).classList.add("active");
+    event.currentTarget.classList.add("active");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementsByClassName("tablink")[0].click();
+});
